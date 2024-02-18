eac581afcbcb13f8200ff27535c6b6145b831a56
Robert Thau <rst@alum.mit.edu>
2024-02-18 12:01:23 -0500

    Driver function in main

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+use std::fs;
+use std::env;
+
+// This is gonna get awfully repetitious...
+pub mod day1;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    let day_num = &args[1];
+    let filename = format!("input/{}", day_num);
+    let input = fs::read_to_string(&filename)
+        .unwrap_or_else(|_| panic!("{filename} not found"));
+
+    match day_num.as_str() {
+        // So is this, but it seems harder to avoid.
+        "1" => day1::day1(&input),
+        _ => println!("no code for {day_num} yet")
+    }
 }
